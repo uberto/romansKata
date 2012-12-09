58471942899bfc43291dd10f2004dcca69dd5f73
Uberto Barbini <uberto.gama@gmail.com>
2012-12-09 22:09:28 +0000

    calculate till 3

--- src/test/java/com/gamasoft/roman/numbers/IntegersToRomansTest.java
+++ src/test/java/com/gamasoft/roman/numbers/IntegersToRomansTest.java
@@ -17,11 +17,26 @@ public class IntegersToRomansTest {
         assertThat(roman(999), is("CMXCIX"));
     }
 
+    @Test
     public void verySimpleIntegersToRomans(){
         assertThat(roman(1), is("I"));
+        assertThat(roman(2), is("II"));
+        assertThat(roman(3), is("III"));
+//        assertThat(roman(4), is("IV"));
+//        assertThat(roman(5), is("V"));
+//        assertThat(roman(6), is("VI"));
+//        assertThat(roman(7), is("VII"));
+//        assertThat(roman(8), is("VIII"));
     }
 
     private String roman(int number) {
-        return "I";
+        int remaining = number;
+        StringBuilder res = new StringBuilder();
+        while (remaining > 0){
+            res.append("I");
+            remaining--;
+        }
+
+        return res.toString();
     }
 }
