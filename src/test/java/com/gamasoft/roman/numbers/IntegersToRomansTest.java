08da3d819530edc34421143e7282e0d49df16866
Uberto Barbini <uberto.gama@gmail.com>
2012-12-09 22:12:34 +0000

    calculate till 8

--- src/test/java/com/gamasoft/roman/numbers/IntegersToRomansTest.java
+++ src/test/java/com/gamasoft/roman/numbers/IntegersToRomansTest.java
@@ -8,8 +8,9 @@ import static org.junit.Assert.assertThat;
 
 public class IntegersToRomansTest {
 
-    @Test @Ignore
-    public void complexIntegersToRomans(){
+    @Test
+    @Ignore
+    public void complexIntegersToRomans() {
         assertThat(roman(123), is("CXII"));
         assertThat(roman(1968), is("MCMLXVIII"));
         assertThat(roman(444), is("CDXLIV"));
@@ -18,23 +19,31 @@ public class IntegersToRomansTest {
     }
 
     @Test
-    public void verySimpleIntegersToRomans(){
+    public void verySimpleIntegersToRomans() {
         assertThat(roman(1), is("I"));
         assertThat(roman(2), is("II"));
         assertThat(roman(3), is("III"));
-//        assertThat(roman(4), is("IV"));
-//        assertThat(roman(5), is("V"));
-//        assertThat(roman(6), is("VI"));
-//        assertThat(roman(7), is("VII"));
-//        assertThat(roman(8), is("VIII"));
+        assertThat(roman(4), is("IV"));
+        assertThat(roman(5), is("V"));
+        assertThat(roman(6), is("VI"));
+        assertThat(roman(7), is("VII"));
+        assertThat(roman(8), is("VIII"));
     }
 
     private String roman(int number) {
         int remaining = number;
         StringBuilder res = new StringBuilder();
-        while (remaining > 0){
-            res.append("I");
-            remaining--;
+        while (remaining > 0) {
+            if (remaining >= 5) {
+                res.append("V");
+                remaining -= 5;
+            } else if (remaining == 4) {
+               res.append("IV");
+                remaining -= 4;
+            } else {
+                res.append("I");
+                remaining--;
+            }
         }
 
         return res.toString();
